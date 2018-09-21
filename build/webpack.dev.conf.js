932cd286020df938261e7d7926a06337f054515d
Ben <ben@jkweixin.com>
2018-09-21 12:32:31 +0800

    fix some bugs and add some config

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -85,7 +85,7 @@ module.exports = new Promise((resolve, reject) => {
       // Add FriendlyErrorsPlugin
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
         compilationSuccessInfo: {
-          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+          messages: [`THE APPLICATION SERVER PORT IS ${port}`],
         },
         onErrors: config.dev.notifyOnErrors
         ? utils.createNotifierCallback()
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -56,7 +56,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new OptimizeCSSPlugin({
       cssProcessorOptions: config.build.productionSourceMap
         ? { safe: true, map: { inline: false } }
-        : { safe: true }
+        : { safe: true, autoprefixer: { remove: false }  }
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
